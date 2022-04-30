ffbc389364a598628558dccc5104e182d750e96e
zach-lau <zlau2000@gmail.com>
2022-04-30 12:22:56 -0600

    Add helper to unzip SELF folders

---
+++ unzip.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+"""
+Unzip the given folder
+"""
+
+from parse import *
+
+if __name__ == "__main__":
+    zipfolder = "/home/zachary/Desktop/ESS/SELF/Applications/Feb"
+    unzip(zipfolder)
